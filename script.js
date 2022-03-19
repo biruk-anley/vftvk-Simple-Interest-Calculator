74b77fa874d5c00c1fa738a73961664345d8f55d
biruk <birukanley2022@gmail.com>
2022-03-19 22:44:32 +0300

    js files

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interst = principal * rate * years;
+    var year = new Date().getFullYear() + parseInt(years);
+
+    document.getElementById("result"), innerHTML =
+        
+        "If you deposit " + principal + "at the rate of " + rate + 
+        
+        "you will recieve " + interst + "in the year" + years 
+        
+        ""
+
+}
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerHTML = rateval;
 }
         
