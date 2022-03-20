cd37d04aca260d8ac1df06cc1edbbb7c65b16fec
biruk <birukanley2022@gmail.com>
2022-03-21 00:01:17 +0300

    add javasript files

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 function compute()
 {
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interst = principal * rate * years;
-    var year = new Date().getFullYear() + parseInt(years);
-
-    document.getElementById("result"), innerHTML =
-        
-        "If you deposit " + principal + "at the rate of " + rate + 
-        
-        "you will recieve " + interst + "in the year" + years 
-        
-        ""
-
+    p = document.getElementById("principal").value;
+    if (p<=0){
+    	alert("Please enter a positive number")
+        document.getElementById("principal").focus()
+	return
+    }
+    r = document.getElementById("rate").value;
+    y = document.getElementById("years").value;
+    re = document.getElementById("result");
+    i = parseFloat(p)*parseFloat(r)*parseFloat(y)/100
+	if (isNaN(i)){
+		alert("Invalid Input")
+	} else{
+		document.getElementById("principal_").innerHTML = p
+		document.getElementById("rate_").innerHTML = r
+		document.getElementById("result").innerHTML = i
+		document.getElementById("year_").innerHTML = new Date().getFullYear()+parseFloat(y)
+		re.classList.add("format")
+		document.querySelector(".result").classList.remove('hide')
+	}
 }
 
-function updateRate() {
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerHTML = rateval;
+function updateTextInput(val){
+	document.getElementById('per').innerHTML=(val+"%");
+	if ((!isNaN(document.getElementById("principal").value)) && !(document.querySelector(".result").classList.contains('hide')))
+		compute();
 }
         
